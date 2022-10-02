0e3a4c4dcd46a1c96d77686da6fda746f35e2c70
Markus Ippy <Markus.Ippy1981@gmail.com>
2022-10-02 08:46:30 +0200

    Uncomment PowerTune update script

--- recipes-qt/powertune/files/powertune-update.sh
+++ recipes-qt/powertune/files/powertune-update.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-#if [ -d /home/pi/build ];then
-#  rm -r /opt/PowerTune
-#  mkdir /opt/PowerTune
-#  chown pi:pi /opt/PowerTune
-#  rsync -r /home/pi/build/. /opt/PowerTune
-#  rm -r /home/pi/build
-# fi
+if [ -d /home/pi/build ];then
+  rm -r /opt/PowerTune
+  mkdir /opt/PowerTune
+  chown pi:pi /opt/PowerTune
+  rsync -r /home/pi/build/. /opt/PowerTune
+  rm -r /home/pi/build
+fi
